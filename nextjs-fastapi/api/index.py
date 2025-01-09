d1603f1fd78751a45759c372eead965d88bd460f
lbholmes <lbholmes1998@gmail.com>
2025-01-09 14:40:17 +0000

    use cache dataclass, check expiry

--- nextjs-fastapi/api/index.py
+++ nextjs-fastapi/api/index.py
@@ -1,26 +1,29 @@
-from fastapi import FastAPI, BackgroundTasks
-from .web_scraper import scrape_stock_page_async
-from .cache import get_cache, set_cache
+from fastapi import FastAPI
+from .stock_scraper import scrape_stock_page_async
+from .cache import StockStashCache
 
 ### Create FastAPI instance with custom docs and openapi url
 app = FastAPI(docs_url="/api/py/docs", openapi_url="/api/py/openapi.json")
+cache = StockStashCache()
 
 @app.get("/api/py/helloFastApi")
 def hello_fast_api():
     return {"message": "Hello from FastAPI"}
 
 @app.get("/api/py/stockData/{ticker}")
-async def fetch_stock_data(ticker: str, background_tasks: BackgroundTasks):
+async def fetch_stock_data(ticker: str):
     """
         Fetch stock data. Use cache if available else scrape in background.
     """
-    cached_data = get_cache(ticker)
+    # cached_data = get_cache(ticker)
+    cached_data = cache.get(ticker)
     if cached_data:
         return {"data": cached_data, "from_cache": True}
     
     # If data isnt cached, scrape the page, cache it, and return data
+    print("Requesting new data from Google Finance")
     stock_data = await scrape_stock_page_async(ticker)
 
-    set_cache(ticker, stock_data)
+    cache.set(ticker, stock_data)
 
     return {"data": stock_data, "from_cache": False}
