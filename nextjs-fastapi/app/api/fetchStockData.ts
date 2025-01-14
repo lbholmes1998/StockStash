0c465e6475528665d3152c46f0be9fcd5e7cbcc4
lbholmes <lbholmes1998@gmail.com>
2025-01-14 16:30:24 +0000

    minor update

--- nextjs-fastapi/app/api/fetchStockData.ts
+++ nextjs-fastapi/app/api/fetchStockData.ts
@@ -1,7 +1,11 @@
 
 export default async function fetchStockData(ticker: string) {
     // TODO - CREATE .env file to hold URLs!
-    const data = await fetch(`http://localhost:3000/api/py/stockData/${ticker}`)
-    const stockData = await data.json()
+    // next fetch is CACHED BY DEFAULT!!
+    const res = await fetch(`http://localhost:3000/api/py/stockData/${ticker}`, {cache: 'no-store'})
+    if (!res.ok) {
+        throw new Error('Failed to fetch stock data')
+    }
+    const stockData = await res.json()
     return stockData
 }
