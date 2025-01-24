53d5a45a619bc2e813dd5d2ea57c9d379467e6bb
lbholmes <lbholmes1998@gmail.com>
2025-01-24 15:33:43 +0000

    attempt to fix fetch error

--- app/api/fetchStockData.ts
+++ app/api/fetchStockData.ts
@@ -2,10 +2,21 @@
 export default async function fetchStockData(ticker: string) {
     // TODO - CREATE .env file to hold URLs!
     // next fetch is CACHED BY DEFAULT!!
-    const res = await fetch(`http://localhost:3000/api/py/stockData/${ticker}`, {cache: 'no-store'})
-    if (!res.ok) {
-        throw new Error('Failed to fetch stock data')
+
+    if (process.env.NODE_ENV === "development") {
+        const res = await fetch(`http://localhost:3000/api/py/stockData/${ticker}`, {cache: 'no-store'})
+        if (!res.ok) {
+            throw new Error('Failed to fetch stock data')
+        }
+        const stockData = await res.json()
+        return stockData
+    } else {
+
+        const res = await fetch(`/api/py/stockData/${ticker}`, {cache: 'no-store'})
+        if (!res.ok) {
+            throw new Error('Failed to fetch stock data')
+        }
+        const stockData = await res.json()
+        return stockData
     }
-    const stockData = await res.json()
-    return stockData
 }
