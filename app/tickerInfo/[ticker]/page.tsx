bed692132e47a5319a3b51af9f205b72c437388e
lbholmes <lbholmes1998@gmail.com>
2025-01-24 15:16:24 +0000

    fix type error

--- app/tickerInfo/[ticker]/page.tsx
+++ app/tickerInfo/[ticker]/page.tsx
@@ -3,10 +3,13 @@ import StockInfo from "@/app/components/StockInfo"
 import fetchStockData from "@/app/api/fetchStockData"
 
 
-export default async function Page({ params }: { params: { ticker: string} }) {
+export default async function Page(props: { params: Promise<{ ticker: string}> }) {
+
+    const params = await props.params
+    const ticker = params.ticker
 
     // Fetch stock data then pass to stock info component.
-    const stockData = await fetchStockData(params.ticker)
+    const stockData = await fetchStockData(ticker)
 
     return (
         <>
