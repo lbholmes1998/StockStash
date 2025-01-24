b08ae312489e6b40ed07450522fc693c31f3896b
lbholmes <lbholmes1998@gmail.com>
2025-01-24 15:56:42 +0000

    properly configure .env variables and use in fetch

--- app/api/fetchStockData.ts
+++ app/api/fetchStockData.ts
@@ -3,20 +3,11 @@ export default async function fetchStockData(ticker: string) {
     // TODO - CREATE .env file to hold URLs!
     // next fetch is CACHED BY DEFAULT!!
 
-    if (process.env.NODE_ENV === "development") {
-        const res = await fetch(`http://localhost:3000/api/py/stockData/${ticker}`, {cache: 'no-store'})
-        if (!res.ok) {
-            throw new Error('Failed to fetch stock data')
-        }
-        const stockData = await res.json()
-        return stockData
-    } else {
-
-        const res = await fetch(`/api/py/stockData/${ticker}`, {cache: 'no-store'})
-        if (!res.ok) {
-            throw new Error('Failed to fetch stock data')
-        }
-        const stockData = await res.json()
-        return stockData
+    const url = `${process.env.NEXT_PUBLIC_URL}/api/py/stockData`
+    const res = await fetch(`${url}/${ticker}`, {cache: 'no-store'})
+    if (!res.ok) {
+        throw new Error('Failed to fetch stock data')
     }
+    const stockData = await res.json()
+    return stockData
 }
